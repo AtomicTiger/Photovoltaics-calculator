c12a4abe56e5b6672bcdc56223cec7e59954885e
unknown <200417pepe@gmail.com>
2023-03-23 10:58:22 +0100

    make a proper code wchich will operate on servers data and try to sum a watts of power gain

--- fotowoltaika/src/components/output.js
+++ fotowoltaika/src/components/output.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import data from "./response.json"
 
 function Output(props) {
     const generate_power = () =>{
@@ -22,16 +23,54 @@ function Output(props) {
             }
         }
         console.log("qwe");
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
     }
+    // conected with props.data
+    // const options = {
+    //     method: 'GET',
+    //     url: 'https://solarenergyprediction.p.rapidapi.com/v2.0/solar/prediction',
+    //     params: {
+    //       lat: props.data[0].lat,
+    //       lon: props.data[0].lon,
+    //       loss: '14',
+    //       plant: '0xAb425D74eA0113d11c3092E47a4C0542C25973db',
+    //       deg: props.data[0].deg,
+    //       az: props.data[0].az,
+    //       wp: props.data[0].wp,
+    //       inverter: '4500',
+    //       decoration: 'default'
+    //     },
+    //     headers: {
+    //       'X-RapidAPI-Key': '',
+    //       'X-RapidAPI-Host': 'solarenergyprediction.p.rapidapi.com'
+    //     }
+    //   }
+    
+    //   axios.request(options)
+    //     .then(function (response) {
+    //       console.log(response.data);
+    //     })
+    //     .catch(function (error) {
+    //       console.error(error);
+    //     });
+    // }
+
   return (
     <div>
+        <br></br>
+        <br></br>
+        <br></br>
         <button onClick={generate_power}>How much power will you get ?</button>
+        <h1>You will gain</h1>
+        <br/>
+        <h1></h1>
+        <br/>
+        <h1> Wats of power</h1>
+
     </div>
   );
 }
